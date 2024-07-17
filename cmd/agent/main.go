89f4bce1a6e19150910600a4da9ffc1a99c3787a
nbuzmakov <oldhanasong@yandex.ru>
2024-07-17 11:29:16 +0400

    2.0.0 Инкремент 2. Реализован агент.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"github.com/oldhanasong/go-metrics-collector/internal/buisness_logic/manager"
+	"github.com/oldhanasong/go-metrics-collector/internal/service/metric/data_collector"
+	"time"
+)
+
+func main() {
+	var n time.Duration = 2
+
+	for {
+		metricManager := manager.MetricManager{MetricDataCollector: data_collector.MetricDataCollector{}}
+		metricManager.CollectAndSend()
+
+		time.Sleep(n * time.Second)
+	}
+}
---
+++ internal/buisness_logic/service/parser/strategy/random_value_strategy.go
@@ -0,0 +1,16 @@
+package strategy
+
+import (
+	"github.com/oldhanasong/go-metrics-collector/internal/dictionary"
+	"github.com/oldhanasong/go-metrics-collector/internal/dto"
+)
+
+type RandomValueStrategy struct{}
+
+func (strategy *RandomValueStrategy) GetMetric(metricName string, metricData dto.CollectedMetricData) dto.Metric {
+	return dto.Metric{
+		Type:  dictionary.GaugeMetricType,
+		Name:  metricName,
+		Value: metricData.RandomValue,
+	}
+}
