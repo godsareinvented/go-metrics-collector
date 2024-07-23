3dece7b098d6fdfe3ffca9b39f72edfd3d95198e
nbuzmakov <oldhanasong@yandex.ru>
2024-07-23 11:01:07 +0400

    3.0.1 Правка бага: После выгрузки метрики из хранилища парсер json'а всегда конвертировал значение метрики к float64.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -27,6 +27,10 @@ func (repository *Repository[Num]) GetMetric(metric dto.Metric[Num]) (dto.Metric
 	var metricDTO dto.Metric[Num]
 	err := json.Unmarshal(jsonMetric.([]uint8), &metricDTO)
 
+	// Необходимо для преобразования значения метрики к корректному (согласно типу метрики),
+	// т.к. парсер json'а распознаёт любое значение как float64
+	metricDTO.Value = Num(metricDTO.Value)
+
 	if nil != err {
 		panic("Cannot unmarshal metric")
 	}
