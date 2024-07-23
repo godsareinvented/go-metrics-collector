b693645fde0fd96b8d244926e173575c45669d93
nbuzmakov <oldhanasong@yandex.ru>
2024-07-23 14:13:03 +0400

    3.1.0 Рефакторинг: Структурам обработчиков значений метрик на сервере даны более корректные названия.

--- internal/buisness_logic/service/value_handler/handler/counter_value_handler.go
+++ internal/buisness_logic/service/value_handler/handler/counter_value_handler.go
@@ -6,11 +6,11 @@ import (
 	"github.com/oldhanasong/go-metrics-collector/internal/repository"
 )
 
-type CounterValuePreprocessor[Num constraint.Numeric] struct {
+type CounterValueHandler[Num constraint.Numeric] struct {
 	Repository *repository.Repository[Num]
 }
 
-func (preprocessor *CounterValuePreprocessor[Num]) GetMutatedValueMetric(metric dto.Metric[Num]) dto.Metric[Num] {
+func (preprocessor *CounterValueHandler[Num]) GetMutatedValueMetric(metric dto.Metric[Num]) dto.Metric[Num] {
 	currentMetricFromDb, isSet := preprocessor.Repository.GetMetric(metric)
 	if isSet {
 		metric.Value += currentMetricFromDb.Value
