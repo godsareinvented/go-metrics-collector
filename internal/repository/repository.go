2e246cbd7ffcc3e5e90593d2d64fa3c613505be5
nbuzmakov <oldhanasong@yandex.ru>
2024-10-29 18:00:07 +0400

    7.0.2 Правка бага: При выводе списка метрик в html все метрики имели одинаковые значения.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -35,12 +35,12 @@ func (repository *Repository) GetMetric(metric dto.Metrics) (dto.Metrics, bool) 
 
 func (repository *Repository) GetAllMetrics() []dto.Metrics {
 	var resultingList []dto.Metrics
-	var metricDTO dto.Metrics
 
 	metricJsonList := (*repository.storage).GetAll()
 	for _, metricJson := range metricJsonList {
-		_ = json.Unmarshal(metricJson.([]byte), &metricDTO)
-		resultingList = append(resultingList, metricDTO)
+		var metric dto.Metrics
+		_ = json.Unmarshal(metricJson.([]byte), &metric)
+		resultingList = append(resultingList, metric)
 	}
 
 	return resultingList
