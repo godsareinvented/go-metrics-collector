ad67aa66c4a3429a54bfc0d444643a46c3285069
nbuzmakov <oldhanasong@yandex.ru>
2024-10-31 14:37:36 +0400

    8.0.0 Унификация миддлевейра логер.

--- internal/server/middleware/logger.go
+++ internal/server/middleware/logger.go
@@ -7,37 +7,39 @@ import (
 )
 
 type responseData struct {
-	statusCode   int
-	responseSize int
+	statusCode   *int
+	responseSize *int
 }
 
 type loggingResponseWriter struct {
-	responseWriter http.ResponseWriter
-	responseData   responseData
-}
-
-func (lrw *loggingResponseWriter) WriteHeader(statusCode int) {
-	lrw.responseData.statusCode = statusCode
-	lrw.responseWriter.WriteHeader(statusCode)
+	http.ResponseWriter
+	responseData responseData
 }
 
 func (lrw *loggingResponseWriter) Write(bytes []byte) (int, error) {
-	lrw.responseData.responseSize += len(bytes)
-	return lrw.responseWriter.Write(bytes)
+	size, err := lrw.ResponseWriter.Write(bytes)
+	if nil == err {
+		*lrw.responseData.responseSize += size
+	}
+	return size, err
 }
 
-func (lrw *loggingResponseWriter) Header() http.Header {
-	return lrw.responseWriter.Header()
+func (lrw *loggingResponseWriter) WriteHeader(statusCode int) {
+	*lrw.responseData.statusCode = statusCode
+	lrw.ResponseWriter.WriteHeader(statusCode)
 }
 
 func WithLogging(handlerFunc http.Handler) http.Handler {
 	fn := func(responseWriter http.ResponseWriter, request *http.Request) {
 		start := time.Now()
 
+		statusCode := http.StatusOK
+		responseSize := 0
 		lrw := loggingResponseWriter{
-			responseWriter: responseWriter,
+			ResponseWriter: responseWriter,
 			responseData: responseData{
-				responseSize: 0,
+				responseSize: &responseSize,
+				statusCode:   &statusCode,
 			},
 		}
 
@@ -48,9 +50,9 @@ func WithLogging(handlerFunc http.Handler) http.Handler {
 		config.Configuration.Logger.Sugar().Infoln(
 			"uri", request.RequestURI,
 			"method", request.Method,
-			"status", lrw.responseData.statusCode,
+			"status", *lrw.responseData.statusCode,
 			"duration", since,
-			"size", lrw.responseData.responseSize,
+			"size", *lrw.responseData.responseSize,
 		)
 	}
 	return http.HandlerFunc(fn)
