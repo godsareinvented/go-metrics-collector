b93205c37d23ea6aeb1de3636b72f28109a5eb3a
nbuzmakov <oldhanasong@yandex.ru>
2024-11-04 20:17:27 +0400

    9.1.0 Переработан сервер, добавлена возможность штатной остановки сервера для выполнения деинициализации, добавлены коллбеки onStart и onStop.

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+	"errors"
 	"github.com/go-chi/chi"
 	"github.com/godsareinvented/go-metrics-collector/internal/config"
 	"github.com/godsareinvented/go-metrics-collector/internal/server/handler"
@@ -8,31 +10,99 @@ import (
 	"net/http"
 )
 
-type Server struct{}
+type Server struct {
+	server *http.Server
+	router *chi.Mux
+	ctx    *context.Context
+	cancel *context.CancelFunc
 
-func (server *Server) Start() {
-	router := chi.NewRouter()
+	OnStart func() error
+	OnStop  func() error
+}
 
-	router.Use(middleware.WithLogging)
-	router.Use(middleware.GzipRequestDecompressing)
-	router.Use(middleware.GzipResponseCompressing)
+func (s *Server) Start() {
+	s.createAndConfigureRouter()
 
-	router.Route("/update", func(router chi.Router) {
+	s.server = &http.Server{
+		Addr:    config.Configuration.Endpoint,
+		Handler: s.router,
+	}
+
+	s.createContext()
+
+	defer (*s.cancel)()
+
+	serverIsRunning := make(chan bool)
+	go func() {
+		go func() {
+			if err := s.server.ListenAndServe(); nil != err && !errors.Is(err, http.ErrServerClosed) {
+				panic("ListenAndServe: " + err.Error())
+			}
+		}()
+
+		serverIsRunning <- true
+	}()
+
+	select {
+	case <-(*s.ctx).Done():
+		err := s.server.Shutdown(*s.ctx)
+		if nil != err {
+			return
+		}
+	case <-serverIsRunning:
+		s.executeOnStartCallback()
+	}
+}
+
+func (s *Server) Stop() {
+	if nil == s.OnStop {
+		(*s.cancel)()
+		return
+	}
+
+	err := s.OnStop()
+
+	(*s.cancel)()
+
+	if nil != err {
+		panic("OnStop: " + err.Error())
+	}
+}
+
+func (s *Server) createAndConfigureRouter() {
+	s.router = chi.NewRouter()
+
+	s.router.Use(middleware.WithLogging)
+	s.router.Use(middleware.GzipRequestDecompressing)
+	s.router.Use(middleware.GzipResponseCompressing)
+
+	s.router.Route("/update", func(router chi.Router) {
 		router.Post("/", handler.UpdateMetricJson)
 		router.Route("/{type}/{name}/{value}", func(router chi.Router) {
 			router.Post("/", handler.UpdateMetric)
 		})
 	})
-	router.Route("/value", func(router chi.Router) {
+	s.router.Route("/value", func(router chi.Router) {
 		router.Post("/", handler.GetMetricJson)
 		router.Route("/{type}/{name}", func(router chi.Router) {
 			router.Get("/", handler.GetMetric)
 		})
 	})
-	router.Get("/", handler.ShowMetricList)
+	s.router.Get("/", handler.ShowMetricList)
+}
 
-	err := http.ListenAndServe(config.Configuration.Endpoint, router)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
+func (s *Server) createContext() {
+	ctx, cancel := context.WithCancel(context.Background())
+	s.ctx = &ctx
+	s.cancel = &cancel
+}
+
+func (s *Server) executeOnStartCallback() {
+	if nil == s.OnStart {
+		return
+	}
+	if err := s.OnStart(); nil != err {
+		// todo: Не panic.
+		panic("OnStart: " + err.Error())
 	}
 }
