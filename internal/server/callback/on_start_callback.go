e68a15ac3d4fd78ba9163d2990edab11c14cbc6c
nbuzmakov <oldhanasong@yandex.ru>
2024-11-21 06:03:00 +0400

    9.5.0 Инкремент 9. Добавлен експорт метрик в постоянное хранилище при сохранении метрики и по таймеру.

--- internal/server/callback/on_start_callback.go
+++ internal/server/callback/on_start_callback.go
@@ -4,15 +4,37 @@ import (
 	"fmt"
 	"github.com/godsareinvented/go-metrics-collector/internal/config"
 	"github.com/godsareinvented/go-metrics-collector/internal/service/metric/manager"
+	"time"
 )
 
 func OnServerStartedCallback() error {
 	printServerStarted()
+	initExportTask()
 	return importMetricsFromPermanentStorage()
 }
 
 func printServerStarted() {
 	fmt.Println("Server is up and running")
+}
+
+// todo: В будущем обязательно переписать на более надёжную схему.
+func initExportTask() {
+	if config.Configuration.StoreInterval > 0 {
+		go exportTask()
+	}
+}
+
+func exportTask() {
+	metricManager := manager.MetricManager{}
+	ticker := time.NewTicker(time.Duration(config.Configuration.StoreInterval) * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			_ = metricManager.ExportTo(config.Configuration.PermanentStorage)
+		}
+	}
 }
 
 func importMetricsFromPermanentStorage() error {
