ff743a46c596cd953b070ca0667b51d6652c24d8
nbuzmakov <oldhanasong@yandex.ru>
2024-12-09 13:07:51 +0400

    11.4.0 Добавлена таблица uuid и функции для генерации идентификаторов метрик, исправлены мелкие ошибки в миграции (VARCHAR(35), название таблицы metric_types).

--- internal/migration/postgresql/20241207160536_create_db.up.sql
+++ internal/migration/postgresql/20241207160536_create_db.up.sql
@@ -1,16 +1,18 @@
-CREATE TYPE metric_type AS ENUM ('gauge', 'counter');
+CREATE TYPE metric_type_enum AS ENUM ('gauge', 'counter');
 
-CREATE TABLE IF NOT EXISTS metric_types (
-    ID SERIAL PRIMARY KEY,
-    metric_type metric_type NOT NULL UNIQUE
+COMMENT ON TYPE metric_type_enum is 'Тип метрики: gauge или counter';
+
+CREATE TABLE IF NOT EXISTS metric_type (
+    ID VARCHAR(36) PRIMARY KEY,
+    metric_type metric_type_enum NOT NULL UNIQUE
 );
 
-COMMENT ON TABLE metric_types is 'Таблица типов метрик';
-COMMENT ON COLUMN metric_types.ID is 'Индекнтификатор метрики';
-COMMENT ON COLUMN metric_types.metric_type is 'Тип метрики: gauge или counter';
+COMMENT ON TABLE metric_type is 'Таблица типов метрик';
+COMMENT ON COLUMN metric_type.ID is 'Индекнтификатор метрики';
+COMMENT ON COLUMN metric_type.metric_type is 'Тип метрики: gauge или counter';
 
 CREATE TABLE IF NOT EXISTS metric (
-    ID SERIAL PRIMARY KEY,
+    ID VARCHAR(36) PRIMARY KEY,
     metric_name VARCHAR(100) NOT NULL,
     delta INTEGER NULL,
     value DOUBLE PRECISION NULL,
@@ -18,7 +20,7 @@ CREATE TABLE IF NOT EXISTS metric (
         (delta IS NOT NULL AND value IS NULL) OR
         (delta IS NULL AND value IS NOT NULL)
     ),
-    CONSTRAINT fk_metric_type FOREIGN KEY (ID) REFERENCES metric_types(ID) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT fk_metric_type FOREIGN KEY (ID) REFERENCES metric_type(ID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 COMMENT ON TABLE metric is 'Таблица метрик';
@@ -26,3 +28,68 @@ COMMENT ON COLUMN metric.ID is 'Индекнтификатор метрики';
 COMMENT ON COLUMN metric.metric_name is 'Название метрики';
 COMMENT ON COLUMN metric.delta is 'Целочисленное значение метрики';
 COMMENT ON COLUMN metric.value is 'Вещественное значение метрики';
+
+CREATE TABLE IF NOT EXISTS uuid (
+    UUID VARCHAR(36) UNIQUE,
+    is_free BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+COMMENT ON TABLE uuid is 'Таблица для генерации уникальных идентификаторов метрик';
+COMMENT ON COLUMN uuid.UUID is 'Уникальный индекнтификатор метрики';
+COMMENT ON COLUMN uuid.is_free is 'Флаг: свободен ли UUID';
+COMMENT ON COLUMN uuid.created_at is 'Timestamp создания UUID';
+
+CREATE OR REPLACE FUNCTION get_first_free_generated_metric_UUID()
+RETURNS VARCHAR(36)
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    searched_UUID VARCHAR(36);
+BEGIN
+    SELECT uuid
+    INTO searched_UUID
+    FROM uuid
+    WHERE is_free = true
+    ORDER BY created_at ASC
+    LIMIT 1;
+    return searched_UUID;
+END;
+$$;
+
+COMMENT ON FUNCTION get_first_free_generated_metric_UUID is 'Функция, возвращающая первый свободный сгенерированный UUID для метрики';
+
+CREATE OR REPLACE FUNCTION get_metric_uuid()
+RETURNS VARCHAR(36)
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    searched_UUID VARCHAR(36);
+	UUID_from_table VARCHAR(36);
+    is_set BOOLEAN;
+BEGIN
+    SELECT get_first_free_generated_metric_UUID() INTO searched_UUID;
+    IF searched_UUID IS NOT NULL THEN
+        return searched_UUID;
+    END IF;
+
+    FOR i IN 1..10 LOOP
+        SELECT FALSE INTO is_set;
+        WHILE NOT is_set LOOP
+            SELECT gen_random_uuid() INTO searched_UUID;
+            BEGIN
+                INSERT INTO uuid("UUID") VALUES (searched_UUID);
+                EXCEPTION WHEN OTHERS THEN
+                    CONTINUE;
+            END;
+            SELECT TRUE INTO is_set;
+        END LOOP;
+    END LOOP;
+
+    return searched_UUID;
+END;
+$$;
+
+COMMENT ON FUNCTION get_metric_uuid is 'Функция, возвращающая первый свободный сгенерированный UUID для метрики. При его отсутствии добавлет в таблицу 10 новых свободных UUID';
