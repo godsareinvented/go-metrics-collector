4f7bc5dffa7b94d517bcd3f4e633dcf2daeb00fa
nbuzmakov <oldhanasong@yandex.ru>
2024-12-11 01:14:08 +0400

    11.5.1 Правка бага: Скорректированы запросы в миграции, чтобы была возможность отката миграции.

--- internal/migration/postgresql/20241207160536_create_db.down.sql
+++ internal/migration/postgresql/20241207160536_create_db.down.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS metric;
 DROP TABLE IF EXISTS metric_type;
+DROP TABLE IF EXISTS metric;
 DROP TABLE IF EXISTS uid;
 DROP TYPE IF EXISTS metric_type_enum;
 DROP FUNCTION IF EXISTS get_first_free_generated_metric_uid;
