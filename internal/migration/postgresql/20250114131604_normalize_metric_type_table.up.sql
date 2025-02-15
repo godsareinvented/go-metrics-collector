c051aceb81cb744ec2d407d611731e07d87d066e
nbuzmakov <oldhanasong@yandex.ru>
2025-02-15 14:11:35 +0400

    12.3.2 Правка бага: Исправлены ошибки в миграции 20250114131604_normalize_metric_type_table.up для сохранения идемпотентности.

--- internal/migration/postgresql/20250114131604_normalize_metric_type_table.up.sql
+++ internal/migration/postgresql/20250114131604_normalize_metric_type_table.up.sql
@@ -3,17 +3,22 @@
 -- (Потому что старый строковый uuid не приести к новому типу поля-идентификатора: INT)
 -- Здесь такой подход игнорируется, потому что данные можно спокойно удалить.
 
-DELETE FROM metric_type;
+-- todo: Как будто, слишком много транзакцй в дешборде pgadmin при старте сервера.
+
 DELETE FROM metric;
+DELETE FROM metric_type;
 
 ALTER TABLE IF EXISTS metric_type
     ALTER COLUMN id SET DATA TYPE INT USING (id::INT),
+    ALTER COLUMN id DROP IDENTITY,
     ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY,
+    DROP CONSTRAINT IF EXISTS unique_metric_type,
     ADD CONSTRAINT unique_metric_type UNIQUE (metric_type),
     DROP CONSTRAINT IF EXISTS fk_metric_type;
 
 ALTER TABLE IF EXISTS metric
-    ADD COLUMN metric_type_id INT NOT NULL REFERENCES metric_type(id),
+    ADD COLUMN IF NOT EXISTS metric_type_id INT NOT NULL REFERENCES metric_type(id),
+    DROP CONSTRAINT IF EXISTS fk_metric,
     ADD CONSTRAINT fk_metric FOREIGN KEY (metric_type_id) REFERENCES metric_type(id) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 COMMENT ON COLUMN metric.metric_type_id is 'Индекнтификатор типа метрики из таблицы metric_type';
