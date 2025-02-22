ae428339a51b008aa3f1812e772ff130eea09bd3
nbuzmakov <oldhanasong@yandex.ru>
2025-02-23 01:42:46 +0400

    14.5.0 Рефакторинг: Убран лишний элиас импорта после разделения кода на серверную и агенсткую часть.

--- internal/server/buisness_logic/value_handler/abstract_factory.go
+++ internal/server/buisness_logic/value_handler/abstract_factory.go
@@ -3,16 +3,16 @@ package value_handler
 import (
 	"github.com/godsareinvented/go-metrics-collector/internal/general/dictionary"
 	"github.com/godsareinvented/go-metrics-collector/internal/general/dto"
-	handler2 "github.com/godsareinvented/go-metrics-collector/internal/server/buisness_logic/value_handler/handler"
+	"github.com/godsareinvented/go-metrics-collector/internal/server/buisness_logic/value_handler/handler"
 	"github.com/godsareinvented/go-metrics-collector/internal/server/interfaces"
 )
 
 func GetValueHandler(metric dto.Metrics) interfaces.ValueHandlerInterface {
 	switch metric.MType {
 	case dictionary.GaugeMetricType:
-		return &handler2.GaugeValueHandler{}
+		return &handler.GaugeValueHandler{}
 	case dictionary.CounterMetricType:
-		return &handler2.CounterValueHandler{}
+		return &handler.CounterValueHandler{}
 	default:
 		panic("unknown metric type")
 	}
