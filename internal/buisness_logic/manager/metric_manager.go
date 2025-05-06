143a830fbe7cbdbb04b57c6fe6bb162e7ada8fbc
nbuzmakov <oldhanasong@yandex.ru>
2025-05-06 23:36:56 +0400

    2.2.3 Правка бага: Для соответствия ТЗ 2-о инкремента сбор и отправка метрик в агенте разделены, добавлены слипы на соответствующие интервалы.

--- internal/buisness_logic/manager/metric_manager.go
+++ internal/buisness_logic/manager/metric_manager.go
@@ -6,26 +6,54 @@ import (
 	valueHandlerAbstractFactory "github.com/oldhanasong/go-metrics-collector/internal/buisness_logic/service/value_handler/abstract_factory"
 	"github.com/oldhanasong/go-metrics-collector/internal/dictionary"
 	"github.com/oldhanasong/go-metrics-collector/internal/dto"
-	"github.com/oldhanasong/go-metrics-collector/internal/interfaces"
 	"github.com/oldhanasong/go-metrics-collector/internal/repository"
 	"github.com/oldhanasong/go-metrics-collector/internal/service/metric/data_collector"
 	"net/http"
 	"reflect"
+	"time"
 )
 
 type MetricManager struct {
 	MetricDataCollector data_collector.MetricDataCollector
 }
 
-func (metricManager *MetricManager) CollectAndSend() {
-	var strategy interfaces.ParsingStrategy
-	var metricDTO dto.Metric
+const (
+	pollInterval   = time.Second * 2
+	reportInterval = time.Second * 10
+)
 
-	metricCollectedData := metricManager.MetricDataCollector.GetMetricData()
-	for _, metricName := range dictionary.MetricNameList {
-		strategy = parserAbstractFactory.GetStrategy(metricName)
-		metricDTO = strategy.GetMetric(metricName, metricCollectedData)
-		metricManager.sendMetrics(metricDTO)
+var (
+	metricList []dto.Metric
+)
+
+func (metricManager *MetricManager) CollectAndSend() {
+	go metricManager.collect()
+	go metricManager.send()
+	select {}
+}
+
+func (metricManager *MetricManager) collect() {
+	for {
+		metricCollectedData := metricManager.MetricDataCollector.GetMetricData()
+
+		metricList = []dto.Metric{}
+		for _, metricName := range dictionary.MetricNameList {
+			strategy := parserAbstractFactory.GetStrategy(metricName)
+			metrics := strategy.GetMetric(metricName, metricCollectedData)
+			metricList = append(metricList, metrics)
+		}
+
+		time.Sleep(pollInterval)
+	}
+}
+
+func (metricManager *MetricManager) send() {
+	for {
+		for _, metrics := range metricList {
+			metricManager.sendMetrics(metrics)
+		}
+
+		time.Sleep(reportInterval)
 	}
 }
 
