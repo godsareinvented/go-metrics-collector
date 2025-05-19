f0e885f5c5d3dfd17729dfe39ce508ffeb5e420a
nbuzmakov <oldhanasong@yandex.ru>
2025-05-19 13:53:50 +0400

    4.2.1 Правка бага: Ендпоинт сервера переведён на ендпоинт из конфига.

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/oldhanasong/go-metrics-collector/internal/config"
 	"github.com/oldhanasong/go-metrics-collector/internal/server/handler"
 	"net/http"
 )
@@ -15,7 +16,7 @@ func (server *Server) Start() {
 	router.Get("/value/{type}/{name}", handler.GetMetric)
 	router.Get("/", handler.ShowMetricList)
 
-	err := http.ListenAndServe("localhost:8080", router)
+	err := http.ListenAndServe(config.Configuration.Endpoint, router)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
