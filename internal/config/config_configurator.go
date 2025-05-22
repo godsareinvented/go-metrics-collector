339a49b12e040c872c357e732c9c66c524825022
nbuzmakov <oldhanasong@yandex.ru>
2025-05-22 22:00:09 +0400

    9.6.1 Рефакторинг: Убрана мелкая ошибка.

--- internal/config/config_configurator.go
+++ internal/config/config_configurator.go
@@ -36,7 +36,6 @@ func (c *ConfigConfigurator) ParseConfig() {
 		flag.IntVar(&Configuration.PollInterval, "p", 2, "Частота опроса метрик из пакета runtime")
 		flag.IntVar(&Configuration.StoreInterval, "i", 300, "Интервал времени в секундах, по истечении которого текущие показания сервера сохраняются на диск")
 		flag.StringVar(&Configuration.FileStoragePath, "f", getFileStoragePathDefaultValue(), "Путь до файла, куда сохраняются текущие значения")
-		flag.StringVar(&Configuration.FileStoragePath, "f", "metric_storage.txt", "Путь до файла, куда сохраняются текущие значения")
 		flag.BoolVar(&Configuration.Restore, "e", true, "Булево значение, определяющее, загружать или нет ранее сохранённые значения из указанного файла при старте сервера")
 
 		flag.Parse()
