22416219ebb6482a2abd9191b98dcb40a758572b
nbuzmakov <oldhanasong@yandex.ru>
2025-06-29 19:20:46 +0400

    14.6.0 Рефакторинг: Удалены неиспользуемые переменные из конфигурации агента.

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -1,22 +1,18 @@
 package config
 
 import (
-	"github.com/oldhanasong/go-metrics-collector/internal/server/interfaces"
-	"github.com/oldhanasong/go-metrics-collector/internal/server/repository"
 	"go.uber.org/zap"
 	"time"
 )
 
 type Config struct {
-	Endpoint                 string                       `env:"ADDRESS"`         // Адрес эндпоинта HTTP-сервера.
-	ReportInterval           time.Duration                `env:"REPORT_INTERVAL"` // Частота отправки метрик на сервер.
-	PollInterval             time.Duration                `env:"POLL_INTERVAL"`   // Частота опроса метрик из пакета runtime.
-	HashKey                  string                       `env:"KEY"`             // Ключ для вычисления хеша.
-	GzipAcceptedContentTypes []string                     // Разрешённые значения для заголовка "Content-Type" при сжатии ответа сервера
-	GzipMinContentLength     int                          // Минимальный размер тела ответа сервера, при котором будет происходить сжатие
-	PermanentStorage         *interfaces.PermanentStorage // Сконфигурированное постоянное хранилище метрик между работой сервера
-	Repository               *repository.Repository       // Сконфигурированный репозиторий
-	Logger                   *zap.Logger                  // Логгер
+	Endpoint                 string        `env:"ADDRESS"`         // Адрес эндпоинта HTTP-сервера.
+	ReportInterval           time.Duration `env:"REPORT_INTERVAL"` // Частота отправки метрик на сервер.
+	PollInterval             time.Duration `env:"POLL_INTERVAL"`   // Частота опроса метрик из пакета runtime.
+	HashKey                  string        `env:"KEY"`             // Ключ для вычисления хеша.
+	GzipAcceptedContentTypes []string      // Разрешённые значения для заголовка "Content-Type" при сжатии ответа сервера
+	GzipMinContentLength     int           // Минимальный размер тела ответа сервера, при котором будет происходить сжатие
+	Logger                   *zap.Logger   // Логгер
 }
 
 var Configuration Config
